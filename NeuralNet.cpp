e8c944ca8c08d3c0f9e2e30a4e585ffd1ea76367
Cody Melthratter <cody.melthratter@sjsu.edu>
2017-12-14 00:16:19 -0800

    Implemented file reading, moved some functions to private section

--- NeuralNet.cpp
+++ NeuralNet.cpp
@@ -2,52 +2,76 @@
 #include "MatrixManipulation.hpp"
 #include <iostream>
 #include <vector>
+#include <fstream>
 
-static bool training = false;
+
+/**
+-----------------------TODO-------------------------------------------------------------------------------------------
+- IMPLEMENT DATA FILE PARSING - MEDIUM
+- IMPLEMENT WEIGHT MATRIX POPULATION - EASY
+- IMPLEMENT INPUT LAYER POPULATION - EASY
+- IMPLEMENT HIDDEN LAYER POPULATION - HARD
+- IMPLEMENT OUTPUT LAYER POPULATION - HARD
+- IMPLEMENT LAYER CALCULATION - HARD
+- IMPLEMENT SIGMOID FUNCTION - HARD
+- IMPLEMENT WEIGHT CORRECTION BACK PROPOGATION - HARD
+- IMPLEMENT ERROR CALCULATION FUNCITON - HARD
+- IMPLEMENT LOGGING - EASY
+----------------------------------------------------------------------------------------------------------------------
+*/
 
 using namespace MatrixUtil;
 
 
 template <typename T>
-struct Node
-{
-	Matrix<T> matrix;
-	T weight;
-};
-
-template <typename T>
 struct Layer 
 {
 
-	vector<Node<T>> data;
+	vector<T> data;
+	void addValues(T value) {
+		data.push_back(value);
+	}
 
 };
 
 int main() {};
 
 
-template <typename T>
 class NeuralNetwork
 {
 
 public:
 
-	NeuralNetwork(Layer<T>& newInputLayer, Layer<T>& newHiddenLayer, Layer<T>& newOutputLayer, T newLearningRate)
+	//default training data file
+	const char *DEFAULT_TRAINING_DATA_FP = "mnist.train.csv";
+
+
+	NeuralNetwork(Layer<float>& newInputLayer, Layer<float>& newHiddenLayer, Layer<float>& newOutputLayer, float newLearningRate)
 	{
 		this->inputLayer = newInputLayer;
 		this->hiddenLayer = newHiddenLayer;
 		this->outputLayer = newOutputLayer;
 		this->learningRate = newLearningRate;
+		setTrainingDataFp(DEFAULT_TRAINING_DATA_FP);
 	}
 
-	float normalize(int value) 
-	{
-		return (float) ((value / 255.0F) * .99F);
-	}	
+	void init() {
+		cout << "Initiating NeuralNetwork";
+		openTrainingFile();
+		cout << "Opened training data file\n";
+		char *line = malloc (sizeof(char*) * 1024);
+		
+
+
+	}
+
+	void setTrainingDataFp(char* filePath) {
+		strcpy (this->trainingDataFp, filePath);
+	}
+
 
 	void train() 
 	{
-
 
 	}
 
@@ -57,10 +81,6 @@ public:
 	}
 
 
-	void parseData() 
-	{
-
-	}
 private:
 
 	Layer<T>& inputLayer;
@@ -68,8 +88,42 @@ private:
 	Layer<T>& outputLayer;
 	Matrix<T>& inputToHiddenLinks;
 	Matrix<T>& hiddenToOutputLinks;
+
+	vector<int> trainingAnswers {};//holds the answer to each sample of data
 	
-	T learningRate;
+	float learningRate;
+
+	char *trainingDataFp;
+	ifstream trainingFile;
+
+
+
+	//parses one line of data from the training file.
+	void parseTrainingData(char* line) 
+	{	
+		cout << "parsing training data\n";
+		trainingAnswers.push_back(atoi(line[0]));
+		for (int i = 1; i < sizeof(line); i++ ) {
+			if (line[i] != ',') inputLayer.addValue(normalizeValue(atoi(line[i])));
+		}
+	}	
+
+	void openTrainingFile() {
+		trainingFile.open(trainingDataFp);
+	}
+
+	//populates the weight value matrix with random normalized values
+	void populateWeightMatrix()
+	{
+
+	}	
+
+	//normalize the value to between 0.01 - 0.99
+	float normalizeValue(int value) 
+	{
+		return (float) ((value / 255.0F) * .99F);
+	}	
+
 
 };
 
